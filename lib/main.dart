d0083eb685c0ee405030f1a441cc19b1eb5fe3dc
iseonghun <matoda4113@gmail.com>
2024-08-17 15:50:57 +0900

    ㅇ

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:logger/logger.dart';
 import 'package:permission_handler/permission_handler.dart';
-
+import 'package:desktop_window/desktop_window.dart';
 import 'controller/data_controller.dart';
+import 'global/global.dart';
 import 'main_page.dart';
 
 void main() async{
@@ -13,7 +15,11 @@ void main() async{
   Get.put(DataController());
   final DataController dataController = Get.find<DataController>();
   await dataController.getPath();
+  Size size = await DesktopWindow.getWindowSize();
+  logger.i(size);
 
+  await DesktopWindow.setMinWindowSize(Size(400,600));
+  await DesktopWindow.setWindowSize(Size(size.width,size.height));
   runApp(const MyApp());
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import desktop_window
 import ffmpeg_kit_flutter
 import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DesktopWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWindowPlugin"))
   FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
